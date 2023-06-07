59e98b07610444439e2bd3d6e309cd65be2ea71a
Snehlth <135414913+Snehlth@users.noreply.github.com>
2023-06-07 10:37:32 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,21 +1,22 @@
 from collections import defaultdict
 
 def group_anagrams(words):
-    anagram=defaultdict(list)
+    anagram=defaultdict(list)               # Dictionary to store anagram groups
     for word in words:
-        key=hash_word(word)
-        anagram[key].append(word)
-    return list(anagram.values())
+        key=hash_word(word)                 # Generate a key for the word
+        anagram[key].append(word)           # Add the word to the corresponding anagram group
+    return list(anagram.values())           # Return the anagram groups as a list
 
 def hash_word(word):
-    count=[0]*26
+    count=[0]*26                            # Frequency count of each letter in the word
     for i in word:
-        count[ord(i)-ord('a')]+=1
-    return tuple(count)
+        count[ord(i)-ord('a')]+=1           # Increment the count for each letter
+    return tuple(count)                     # Return a tuple as the key because it is immutable and hashable
 
 
-words=input().split()
-anagram_sets=[]
-anagram_sets.append(group_anagrams(words))
+
+words=input().split()                       # Take input from the user
+anagram_sets=[]                             # Call the function to group the anagrams
+anagram_sets.append(group_anagrams(words))  # Print the resulting anagram sets
 for i in anagram_sets:
     print(i)
